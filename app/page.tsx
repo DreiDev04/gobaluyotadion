e77c6f4c92aa8eb9e881784ae713176f5c49deef
DreiDev04 <tacujan.andrei@gmail.com>
2025-09-28 12:32:12 +0800

    added: atty janok

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import Hero from '@/components/sections/home/Hero';
-import Introduction from '@/components/sections/home/Introduction';
-import Partners from '@/components/sections/home/Partners';
-import PracticePreview from '@/components/sections/home/PracticeArea';
-import Question from '@/components/sections/home/Question';
-
+import React from "react";
+import Hero from "@/components/sections/home/Hero";
+import Introduction from "@/components/sections/home/Introduction";
+import Partners from "@/components/sections/home/Partners";
+import Question from "@/components/sections/home/Question";
 
 const page = () => {
   return (
@@ -12,7 +10,7 @@ const page = () => {
       <div className="fixed inset-0 w-full">
         <Hero />
       </div>
-    <div className='flex flex-col min-h-screen z-10 relative fade-in'>
+      <div className="flex flex-col min-h-screen z-10 relative fade-in">
         <div className="h-[50dvh] sm:h-screen"></div>
         <div className="w-full bg-background/98 backdrop-blur-xl flex-1">
           <Introduction />
@@ -23,7 +21,7 @@ const page = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
